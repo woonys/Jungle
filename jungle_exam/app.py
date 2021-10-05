805888e283acbf2339d38eacea37ab91e0b22b89
jaewoon kim <jaewoonkim@jaewoonui-MacBookAir.local>
2021-10-05 22:55:05 +0900

    jungle_ver1.1

--- jungle_exam/app.py
+++ jungle_exam/app.py
@@ -13,18 +13,22 @@ def home():
 
 @app.route('/memo/show', methods=['GET'])
 def showing_card():
-    all_memos = list(db.exam.find({}, {'_id': 0}))
-    return jsonify({'result':'success', 'all_memos':all_memos})
+    all_memos = list(db.exam.find({}))
+    all_id = []
+    for memo in all_memos:
+        all_id.append(str(memo['_id']))
+
+    all_withoutid = list(db.exam.find({}, {'_id':0}))
+    return jsonify({'result':'success', 'all_memos':all_withoutid, 'all_id':all_id})
 
 
-@app.route('/memo/open', methods=['GET'])
-def open_card():
-    title_receive = request.args.get('title_give')
-    this_memo_with_id = db.exam.find_one({'title':title_receive})
-    this_id = str(this_memo_with_id['_id'])
-    this_memo = db.exam.find_one({'title': title_receive}, {'_id':0})
-    print(this_id)
-    return jsonify({'result': 'success', 'this_memo': this_memo, 'id': this_id})
+# @app.route('/memo/open', methods=['GET'])
+# def open_card():
+#     title_receive = request.args.get('title_give')
+#     this_memo_with_id = db.exam.find_one({'title':title_receive})
+#     this_id = str(this_memo_with_id['_id'])
+#     this_memo = db.exam.find_one({'title': title_receive}, {'_id':0})
+#     return jsonify({'result': 'success', 'this_memo': this_memo, 'id': this_id})
 
 
 
@@ -43,13 +47,14 @@ def posting_card():
 
 @app.route('/memo/update', methods=['POST'])
 def update_card():
-    id_receive = request.form['id_give']
+    title_before = request.form['title_before_give']
+    # id_receive = request.form['id_give']
     title_receive = request.form['title_give']
     contents_receive = request.form['contents_give']
-    print("id_receive: ", id_receive)
-    print(title_receive, contents_receive)
-    db.exam.update_one({'_id': id_receive}, {'$set': {'title': title_receive}})
-    db.exam.update_one({'_id': id_receive}, {'$set': {'contents': contents_receive}})
+
+    print('title_before:', title_before, 'title_now: ', title_receive, 'contents: ', contents_receive)
+    db.exam.update_one({'title': title_before}, {'$set': {'title': title_receive}})
+    db.exam.update_one({'title': title_receive}, {'$set': {'contents': contents_receive}})
     print(db.exam.find_one({'title':title_receive}))
     return jsonify({'result': 'success', 'msg': '수정 완료'})
 
