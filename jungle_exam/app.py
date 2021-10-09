1b2cfa604362cc4b6c10afe3f4d643faabaf5ff5
woonys <wodns1324@naver.com>
2021-10-09 15:22:20 +0900

    jungle_ver1.3

--- jungle_exam/app.py
+++ jungle_exam/app.py
@@ -1,51 +1,51 @@
 from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+
 app = Flask(__name__)
 
 # mongodb://test:test@
-from pymongo import MongoClient
-client = MongoClient('mongodb://test:test@localhost', 27017)
+client = MongoClient('localhost', 27017)
 db = client.dbjungle
 
-## HTML을 주는 부분
+
+# HTML 을 주는 부분
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/memo/show', methods=['GET'])
-def showing_card():
+
+@app.route('/memos', methods=['GET'])
+def list_cards():
     all_memos = list(db.exam.find({}))
-    all_id = []
     for memo in all_memos:
-        all_id.append(str(memo['_id']))
+        memo['id'] = str(memo.pop('_id'))
+    print(all_memos)
+    return jsonify({'result': 'success', 'all_memos': all_memos})
 
-    all_withoutid = list(db.exam.find({}, {'_id':0}))
-    return jsonify({'result':'success', 'all_memos':all_withoutid, 'all_id':all_id})
 
-
-# @app.route('/memo/open', methods=['GET'])
-# def open_card():
-#     title_receive = request.args.get('title_give')
-#     this_memo_with_id = db.exam.find_one({'title':title_receive})
-#     this_id = str(this_memo_with_id['_id'])
-#     this_memo = db.exam.find_one({'title': title_receive}, {'_id':0})
-#     return jsonify({'result': 'success', 'this_memo': this_memo, 'id': this_id})
-
+@app.route('/memo/{id:str}', methods=['GET'])
+def get_card():
+    title_receive = request.args.get('title_give')
+    this_memo_with_id = db.exam.find_one({'title': title_receive})
+    this_id = str(this_memo_with_id['_id'])
+    this_memo = db.exam.find_one({'title': title_receive}, {'_id': 0})
+    return jsonify({'result': 'success', 'this_memo': this_memo, 'id': this_id})
 
 
 ## API 역할을 하는 부분
-@app.route('/memo/post', methods=['POST'])
-def posting_card():
+@app.route('/memos', methods=['POST'])
+def create_card():
     title_receive = request.form['title_give']
     contents_receive = request.form['contents_give']
 
-    doc = {'title':title_receive, 'contents':contents_receive}
+    doc = {'title': title_receive, 'contents': contents_receive}
 
     db.exam.insert_one(doc)
 
-    return jsonify({'result':'success', 'msg':'저장 완료!'})
+    return jsonify({'result': 'success', 'msg': '저장 완료!'})
 
 
-@app.route('/memo/update', methods=['POST'])
+@app.route('/memos', methods=['PATCH'])  # vs PUT
 def update_card():
     title_before = request.form['title_before_give']
     # id_receive = request.form['id_give']
@@ -55,17 +55,16 @@ def update_card():
     print('title_before:', title_before, 'title_now: ', title_receive, 'contents: ', contents_receive)
     db.exam.update_one({'title': title_before}, {'$set': {'title': title_receive}})
     db.exam.update_one({'title': title_receive}, {'$set': {'contents': contents_receive}})
-    print(db.exam.find_one({'title':title_receive}))
+    print(db.exam.find_one({'title': title_receive}))
     return jsonify({'result': 'success', 'msg': '수정 완료'})
 
 
-
-
-@app.route('/memo/delete', methods=['POST'])
+@app.route('/memos/{id:str}', methods=['DELETE'])
 def delete_card():
     title_receive = request.form['title_give']
     db.exam.delete_one({'title': title_receive})
     return jsonify({'result': 'success', 'msg': '제거되었습니다!'})
 
+
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
